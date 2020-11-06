bb69f2bcca9b82561ab163cc3324b888516cad64
Marc <emarc237@gmail.com>
2020-11-06 04:37:02 +0100

    init commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+// const routes = require("./routes/api");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/affirmedme", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to DB"))
+  .catch((error) => console.error(error));
+
+const app = express();
+
+// Load routes
+app.use("/content", express.static(path.join(__dirname, "content")));
+// app.use("/api", routes);
+
+module.exports = app;
