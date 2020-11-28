bb7a87482cd669d0c57a685d64b1c034578c60ec
Marc <emarc237@gmail.com>
2020-11-28 14:03:29 +0100

    perf(db): switch to online db

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const routes = require('./routes/web');
 const apiRoutes = require('./routes/api');
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/affirmedme", {
+  .connect("mongodb+srv://admin:admin237@affirmedcluster.346yk.mongodb.net/affirmedme?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
