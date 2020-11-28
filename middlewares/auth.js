035fc52ce6e98b7c40953ea445232a2d79ef5222
Marc <emarc237@gmail.com>
2020-11-28 11:55:14 +0100

    perf(auth): add auth middleware on api

---
+++ middlewares/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const Constants = require('../util/constants');
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token)
+    return res.status(403).json({ message: "You don't have required authorization." });
+
+  jwt.verify(token, Constants.TOKEN_SECRET, (err, decoded) => {
+    if (err)
+      return res.status(403).json({ message: "You don't have required authorization." });
+
+      console.log(decoded);
+
+    return next();
+  });
+}
